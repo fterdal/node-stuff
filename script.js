e9e67014cf5aa65e006d7f812eead489fde292e1
Finn <finnterdal@gmail.com>
2019-09-12 15:57:18 -0500

    adds to database

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const fs = require('fs').promises;
+const fs = require('fs').promises
+const Sequelize = require('sequelize')
+const db = new Sequelize('postgres://localhost:5432/dates')
+
+const Date = db.define('date', {
+  date: Sequelize.DATEONLY,
+})
 
 const writeToFile = async () => {
   try {
-    await fs.appendFile('output.txt', (new Date()).toString() + '\n')
+    await db.sync()
+    await Date.create({ date: new Date() })
+    await fs.appendFile(
+      '/Users/finnterdal/code/hello/this/is/where/the/output/is/output.txt',
+      new Date().toString() + '\n'
+    )
+    await db.close()
   } catch (err) {
     console.log('ERR', err)
   }
-
 }
 writeToFile()
